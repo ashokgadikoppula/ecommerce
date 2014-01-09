501244a2d909aa07b2e6c9132888bbce752b1118
ashokgadikoppula <ashokgadikoppula@gmail.com>
2014-01-09 16:44:30 +0530

    frontend

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109054042) do
+ActiveRecord::Schema.define(version: 20140109105103) do
 
   create_table "spree_activators", force: true do |t|
     t.string   "description"
